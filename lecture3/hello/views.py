6f62e015188dd9a64347f799d6289d9da9a3081d
fahad-jamshad-ali <fahad@waltz-ai.com>
2025-08-06 18:07:21 +0500

    Added the templates for views

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World")
+    return render(request, "hello/index.html")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
